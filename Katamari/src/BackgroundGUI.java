92075b68cae10ecc9b16660d3a8f36ba3f0bf597
princekk122 <awuahkarikari12@gmail.com>
2025-04-28 17:35:50 -0500

    add timer countdown to UI

--- Katamari/src/BackgroundGUI.java
+++ Katamari/src/BackgroundGUI.java
@@ -7,42 +7,38 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
-
-public class BackgroundGUI extends JPanel implements ActionListener, Observer
-{
+public class BackgroundGUI extends JPanel implements ActionListener, Observer {
     private static final int WINWIDTH = 640;
     private static final int WINHEIGHT = 480;
 
     private Image leftSquirrelImg;
     private Image rightSquirrelImg;
-   
-    
+
     Player p1;
     Player p2;
-    
+
     PlayerController pC1;
     PlayerController pC2;
-    
+
     Ball ball;
     private ArrayList<Obstacle> obstacles;
-private static final int NUM_OBSTACLES = 30;
+    private static final int NUM_OBSTACLES = 30;
+    private GameTimer gameTimer;
 
-    
-    public BackgroundGUI(Player p1, Player p2, PlayerController pC1, PlayerController pC2) throws URISyntaxException 
-    {	
-		this.p1 = p1;
-		this.p2 = p2;
-		
-		this.pC1 = pC1;
-		this.pC2 = pC2;
-		
-		p1.register(this);
-		p2.register(this);
-		
-		this.ball = new Ball(320, 240, this.p1, this.p2);
-		
-		ball.register(this);
-    	
+    public BackgroundGUI(Player p1, Player p2, PlayerController pC1, PlayerController pC2) throws URISyntaxException {
+        this.p1 = p1;
+        this.p2 = p2;
+
+        this.pC1 = pC1;
+        this.pC2 = pC2;
+
+        p1.register(this);
+        p2.register(this);
+
+        this.ball = new Ball(320, 240, this.p1, this.p2);
+
+        ball.register(this);
+
         setPreferredSize(new Dimension(WINWIDTH, WINHEIGHT));
         setFocusable(true);
         requestFocusInWindow();
@@ -50,31 +46,31 @@ private static final int NUM_OBSTACLES = 30;
         try {
             leftSquirrelImg = ImageIO.read(new URI("https://inventwithpython.com/squirrel.png").toURL());
             rightSquirrelImg = ImageIO.read(new URI("https://inventwithpython.com/squirrel.png").toURL());
-            
+
         } catch (IOException e) {
             System.err.println("Error loading images from the web: " + e.getMessage());
         }
 
-        
-
         obstacles = new ArrayList<>();
-for (int i = 0; i < NUM_OBSTACLES; i++) {
-    obstacles.add(new Obstacle(WINWIDTH, WINHEIGHT));
-}
+        for (int i = 0; i < NUM_OBSTACLES; i++) {
+            obstacles.add(new Obstacle(WINWIDTH, WINHEIGHT));
+        }
 
+        // Starting the timer
+        this.gameTimer = new GameTimer();
+        this.gameTimer.register(this);
+        this.gameTimer.start();
     }
 
-  
-	public void createGame() {
-        SwingUtilities.invokeLater(() -> {   
-        	JFrame frame = new JFrame("Katamari");
-        	
+    public void createGame() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Katamari");
+
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           
-            
+
             this.addKeyListener(pC1);
             this.addKeyListener(pC2);
-            
+
             frame.getContentPane().add(this);
             frame.pack();
             frame.setLocationRelativeTo(null);
@@ -85,21 +81,20 @@ for (int i = 0; i < NUM_OBSTACLES; i++) {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-       
+
         g.setColor(new Color(52, 99, 49));
-        g.fillRect(0, 0, WINWIDTH, WINHEIGHT); 
-        
+        g.fillRect(0, 0, WINWIDTH, WINHEIGHT);
+
         ball.checkCollisions(p1);
         ball.checkCollisions(p2);
-        
-    	g.setColor(Color.RED);
-	    g.fillOval(this.ball.getX(), this.ball.getY(), this.ball.getScale(), this.ball.getScale());
+
+        g.setColor(Color.RED);
+        g.fillOval(this.ball.getX(), this.ball.getY(), this.ball.getScale(), this.ball.getScale());
 
         for (Obstacle o : obstacles) {
             o.draw(g, this);
         }
-        
-	    
+
         if (leftSquirrelImg != null) {
             g.drawImage(leftSquirrelImg, this.p1.getX(), this.p1.getY(), this);
         }
@@ -109,9 +104,9 @@ for (int i = 0; i < NUM_OBSTACLES; i++) {
 
         g.setFont(new Font("Arial", Font.BOLD, 32));
         g.setColor(Color.BLACK);
-    }
 
-    
+        g.drawString(gameTimer.getFormattedTime(), WINWIDTH - 100, 30);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -122,37 +117,33 @@ for (int i = 0; i < NUM_OBSTACLES; i++) {
         for (Obstacle o : obstacles) {
             o.move(WINWIDTH, WINHEIGHT);
         }
-        
+
         checkObstacleCollisions();
-        
-        repaint(); 
+
+        gameTimer.update();
+        repaint();
     }
 
     private void checkObstacleCollisions() {
         boolean ballHeldByPlayer = ball.isHeldBy(p1) || ball.isHeldBy(p2);
-    
+
         if (!ballHeldByPlayer) {
-            return; 
+            return;
         }
-    
+
         ArrayList<Obstacle> collected = new ArrayList<>();
-    
+
         for (Obstacle o : obstacles) {
             if (ball.getBounds().intersects(o.getBounds())) {
                 collected.add(o);
             }
         }
-    
+
         obstacles.removeAll(collected);
     }
 
-  
-    
-    
-    
-
-	@Override
-	public void update() {
-		this.repaint();
-	}
+    @Override
+    public void update() {
+        this.repaint();
+    }
 }
